703405fdcd0284b5ffa6cee19c3820266ff5f9f4
Jérémy Potel <potel.jeremy@gmail.com>
2022-08-06 20:32:47 +0200

    Auto-generated commit [ci skip]

--- src/app/shared/enums/stone.enum.ts
+++
@@ -1,7 +0,0 @@
-export enum StoneEnum {
-  fire = './assets/icons/Fire_Stone.png',
-  water = './assets/icons/Water_Stone.png',
-  thunder = './assets/icons/Thunder_Stone.png',
-  moon = './assets/icons/Moon_Stone.png',
-  leaf = './assets/icons/Leaf_Stone.png'
-}
--- src/app/shared/interfaces/user.ts
+++
@@ -1,7 +0,0 @@
-import {Pokemon} from './pokemon';
-
-export interface User {
-  login: string;
-  password: string;
-  pokemonTeam?: Array<Pokemon>;
-}
