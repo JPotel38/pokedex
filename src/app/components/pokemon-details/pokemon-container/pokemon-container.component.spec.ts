b6b3daba00d15ab5e428f337a6438dea5259e541
Jérémy Potel <potel.jeremy@gmail.com>
2023-07-05 15:51:47 +0200

    WIP PokemonContainer test

--- src/app/components/pokemon-details/pokemon-container/pokemon-container.component.spec.ts
+++ src/app/components/pokemon-details/pokemon-container/pokemon-container.component.spec.ts
@@ -1,8 +1,13 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
+import {IonicModule} from '@ionic/angular';
 
-import { PokemonContainerComponent } from './pokemon-container.component';
+import {PokemonContainerComponent} from './pokemon-container.component';
 import {RouterTestingModule} from "@angular/router/testing";
+import {PokemonDetailsPage} from "../details/pokemon-details.page";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {TranslatePipe, TranslateService} from "@ngx-translate/core";
+import {I18nModule} from "../../../shared/modules/i18n.module";
+import {ActivatedRoute, ActivatedRouteSnapshot} from "@angular/router";
 
 describe('PokemonContainerComponent', () => {
   let component: PokemonContainerComponent;
@@ -10,8 +15,16 @@ describe('PokemonContainerComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ PokemonContainerComponent ],
-      imports: [IonicModule.forRoot(), RouterTestingModule]
+      declarations: [PokemonContainerComponent, PokemonDetailsPage, TranslatePipe],
+      imports: [IonicModule.forRoot(), RouterTestingModule, HttpClientTestingModule, I18nModule],
+      providers: [TranslateService, {
+        provide: ActivatedRoute,
+        useValue: {
+          snapshot: {
+            params: {id: '1'}
+          } as unknown as ActivatedRouteSnapshot,
+        },
+      }]
     }).compileComponents();
 
     fixture = TestBed.createComponent(PokemonContainerComponent);
