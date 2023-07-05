47ef73e1d520d2c30cab7d0ddf3320c227baf0ab
Jérémy Potel <potel.jeremy@gmail.com>
2023-07-05 15:51:32 +0200

    test: update Pokemon name

--- src/app/shared/services/trainer.service.spec.ts
+++ src/app/shared/services/trainer.service.spec.ts
@@ -1,11 +1,12 @@
 import {TestBed} from '@angular/core/testing';
 
 import {TrainerService} from './trainer.service';
-import {mockPokemonList} from "../../tests/mocks/mock-pokemon-team";
+import {mockPokemon, mockPokemonTeam} from "../../tests/mocks/mock-pokemon-team";
 
 describe('TrainerService', () => {
   let trainerService: TrainerService;
-  let pokemonTeam = mockPokemonList
+  let pokemonTeam = mockPokemonTeam;
+  let pokemon = mockPokemon;
 
   const userObject = {
     login: 'login',
@@ -18,23 +19,39 @@ describe('TrainerService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     trainerService = TestBed.inject(TrainerService);
+    trainerService.$user.next(userObject);
+  });
+
+  afterEach(() => {
+    const updatedUserObject = {
+      ...userObject,
+      pokemonTeam: [...mockPokemonTeam]
+    };
+    trainerService.$user.next(updatedUserObject);
   });
 
   it('should be created', () => {
     expect(trainerService).toBeTruthy();
   });
 
-  it('should send user to BehaviorSubject', (done: DoneFn) => {
-    trainerService.$user.next(userObject);
+  it('should send user to BehaviorSubject', () => {
     trainerService.$user.subscribe(value => {
-      expect(value).toEqual({
-        login: 'login',
-        password: 'pass',
-        userName: 'Sacha',
-        connected: true,
-        pokemonTeam: pokemonTeam
-      });
-      done();
+      expect(value).toEqual(userObject);
     });
   });
+
+  it('should add Pokemon to team', () => {
+    trainerService.addPokemonToTeam(pokemon);
+    trainerService.$user.subscribe(value => {
+      expect(value.pokemonTeam).toContain(pokemon);
+    });
+  });
+
+  it('should update Pokemon"s name', () => {
+    trainerService.updatePokemonName(0, "Pikachu");
+    trainerService.$user.subscribe(value => {
+      expect(value.pokemonTeam[0].name).toBe("Pikachu");
+    });
+  });
+
 });
